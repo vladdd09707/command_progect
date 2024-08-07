928840cfd6263b1e722067e69e90050f54305f9f
vladdd09707 <vladdd09707@gmail.com>
2024-08-07 13:35:34 +0200

    Add files via upload

---
+++ TelegramBot/src/main/java/ua/javarush/MyFirstTelegramBot.java
@@ -0,0 +1,38 @@
+package ua.javarush;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Map;
+
+import static ua.javarush.TelegramBotContent.*;
+import static ua.javarush.TelegramBotUtils.*;
+
+public class MyFirstTelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        // TODO: додай ім'я бота в лапки нижче
+        return "";
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO: додай токен бота в лапки нижче
+        return "";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // TODO: основний функціонал бота будемо писати тут
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyFirstTelegramBot());
+    }
+}
